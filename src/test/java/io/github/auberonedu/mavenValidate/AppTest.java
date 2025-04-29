0aee2227c98ae7967268465bc98d94bb2b642321
Auberon López <auberonedu@gmail.com>
2025-04-29 08:46:40 -0700

    Running initial setup script

---
+++ src/test/java/io/github/auberonedu/mavenValidate/AppTest.java
@@ -0,0 +1,11 @@
+package io.github.auberonedu.mavenValidate;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+    @Test
+    void testMainRuns() {
+        assertTrue(true);
+    }
+}
